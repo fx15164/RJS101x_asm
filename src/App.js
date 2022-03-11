959d0890916dbce0f619abc53ab380b5aea82df7
fx15164 <khoannfx15164@funix.edu.vn>
2022-03-11 20:35:13 +0700

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Reac, { Component } from "react";
+import { Navbar, NavbarBrand } from 'reactstrap'
 
 class App extends Component {
 
   render() {
     return (
-      <div></div>
+      <div>
+        <Navbar dark color='primary'>
+          <div className="container">
+            <NavbarBrand>Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
     )
   }
 
