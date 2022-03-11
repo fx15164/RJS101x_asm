4db10d3ffeb662f805bdee35e3d58024972a31d2
fx15164 <khoannfx15164@funix.edu.vn>
2022-03-11 20:50:16 +0700

    Add Staff click event

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -4,6 +4,17 @@ import { Card, CardBody, CardTitle } from 'reactstrap';
 
 class StaffList extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedStaff: null
+        }
+    }
+
+    selectStaff(staff) {
+        this.setState({ selectedStaff: staff });
+        console.log(staff);
+    }
 
     render() {
         const { staffs } = this.props;
@@ -12,7 +23,7 @@ class StaffList extends Component {
                 <div className="row">
                     {staffs.map(staff => (
                         <div key={staff.id} className="col-12 col-sm-6 col-lg-4">
-                            <Card className="m-1">
+                            <Card onClick={() => this.selectStaff(staff)} className="m-1">
                                 <CardBody>
                                     <CardTitle>{staff.name}</CardTitle>
                                 </CardBody>
