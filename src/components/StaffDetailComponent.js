e825a750a9116a98d85eacdb0f7a989663de37a2
fx15164 <khoannfx15164@funix.edu.vn>
2022-03-11 21:00:41 +0700

    Update StaffDetail

--- src/components/StaffDetailComponent.js
+++ src/components/StaffDetailComponent.js
@@ -3,12 +3,15 @@ import { Card, CardBody } from "reactstrap";
 
 class StaffDetail extends Component {
     render() {
-        const { name } = this.props.staff;
+        const { name, doB, startDate, department, annualLeave, overTime } = this.props.staff;
         return (
             <div className="col-12 col-sm-6 col-lg-4 m-1">
                 <Card>
                     <CardBody>
                         <h4>HỌ và tên: {name}</h4>
+                        <p>Phòng ban: {department.name}</p>
+                        <p>Số ngày nghỉ còn lại: {annualLeave}</p>
+                        <p>Số ngày đã làm thêm: {overTime}</p>
                     </CardBody>
                 </Card>
             </div>
