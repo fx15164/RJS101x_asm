f3a339535ff6589ffd8ec403d9f816eff99969f1
fx15164 <khoannfx15164@funix.edu.vn>
2022-03-15 09:47:20 +0700

    Add MainContianer

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
-import React, { Component } from "react";
-import { BrowserRouter, Routes, Route, Navigate, useParams } from 'react-router-dom';
-import Header from "./components/HeaderComponent";
-import StaffList from "./components/StaffListComponent";
-import StaffDetail from './components/StaffDetailComponent';
-import Footer from "./components/FooterComponent";
-import DepartmentList from "./components/DepartmentListComponent";
-import { STAFFS, DEPARTMENTS } from './shared/staffs';
+import React from "react";
+import { BrowserRouter } from 'react-router-dom';
+import MainContainer from './container/MainContainer';
 import './App.css';
-import SalaryTable from "./components/SalaryTableComponent";
 
-class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      staffs: STAFFS,
-      departments: DEPARTMENTS
-    }
-  }
-
-  render() {
-
-    const { staffs, departments } = this.state;
-
-    const StaffWithId = (props) => {
-      const { id } = useParams();
-      return <StaffDetail staff={staffs.filter(staff => staff.id === parseInt(id))[0]} />
-    }
-
-    return (
-      <div>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/nhanvien" element={<StaffList staffs={staffs} />} />
-            <Route path="/nhanvien/:id" element={<StaffWithId />} />
-            <Route path="/phongban" element={<DepartmentList departments={departments} />} />
-            <Route path="/bangluong" element={<SalaryTable staffs={staffs} />} />
-            <Route path="*" element={<Navigate to='/nhanvien' />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <MainContainer />
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/container/MainContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Routes, Route, Navigate, useParams } from 'react-router-dom';
+import Header from "../components/HeaderComponent";
+import StaffList from "../components/StaffListComponent";
+import StaffDetail from '../components/StaffDetailComponent';
+import Footer from "../components/FooterComponent";
+import DepartmentList from "../components/DepartmentListComponent";
+import SalaryTable from "../components/SalaryTableComponent";
+import { STAFFS, DEPARTMENTS } from '../shared/staffs';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            staffs: STAFFS,
+            departments: DEPARTMENTS
+        }
+    }
+
+    render() {
+
+        const { staffs, departments } = this.state;
+
+        const StaffWithId = (props) => {
+            const { id } = useParams();
+            return <StaffDetail staff={staffs.filter(staff => staff.id === parseInt(id))[0]} />
+        }
+
+        return (
+            <div>
+                <Header />
+                <Routes>
+                    <Route path="/nhanvien" element={<StaffList staffs={staffs} />} />
+                    <Route path="/nhanvien/:id" element={<StaffWithId />} />
+                    <Route path="/phongban" element={<DepartmentList departments={departments} />} />
+                    <Route path="/bangluong" element={<SalaryTable staffs={staffs} />} />
+                    <Route path="*" element={<Navigate to='/nhanvien' />} />
+                </Routes>
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default App;
